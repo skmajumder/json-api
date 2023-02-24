9a650d53ade20d048c9feba5c3492c6a137f9dce
skmajumder <skmajumder.cse@gmail.com>
2023-02-24 19:46:39 +0600

    Country details

--- js/countries.js
+++ js/countries.js
@@ -12,17 +12,33 @@ const displayCountries = (countries) => {
   const countriesContainer = document.getElementById("countries-container");
   let i = 1;
   countries.forEach((country) => {
-    console.log(country);
     const countryArticle = document.createElement("article");
     countryArticle.classList.add("country");
     countryArticle.setAttribute("id", `country-${i}`);
     countryArticle.innerHTML = `
-        <p>Country Name: <span id="country-name">${country.name.common}</span></p>
-        <p>Capital: <span id="country-capital">${country.capital}</span></p>
+        <p>Country Name: <span id="country-name">${
+          country.name.common
+        }</span></p>
+        <p>Country Official Name: <span id="country-official-name">${
+          country.name.official
+        }</span></p>
+        <p>Capital: <span id="country-capital">${
+          country.capital ? country.capital[0] : "No Capital"
+        }</span></p>
+        <button onclick="loadCountriesDetails('${
+          country.cca2
+        }')">Show Details</button>
     `;
     countriesContainer.appendChild(countryArticle);
     i++;
   });
+};
+
+const loadCountriesDetails = (code) => {
+  const apiCallURL = `https://restcountries.com/v3.1/alpha/${code}`;
+  fetch(apiCallURL)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 };
 
 loadCountries();
